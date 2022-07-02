2277d2b461bfe6bb3586cc241401c0bab0db2fe7
Taoning Ge <gtn1024o@outlook.com>
2022-07-02 18:57:18 +0800

    移除一些无用组建

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,4 +1,3 @@
-import {outLogin} from '@/services/ant-design-pro/api';
 import {LogoutOutlined, SettingOutlined, UserOutlined} from '@ant-design/icons';
 import {history, useModel} from '@umijs/max';
 import {Avatar, Menu, Spin} from 'antd';
@@ -8,6 +7,7 @@ import type {MenuInfo} from 'rc-menu/lib/interface';
 import React, {useCallback} from 'react';
 import HeaderDropdown from '../HeaderDropdown';
 import styles from './index.less';
+import {setToken} from "@/utils/authority";
 
 export type GlobalHeaderRightProps = {
   menu?: boolean;
@@ -17,7 +17,7 @@ export type GlobalHeaderRightProps = {
  * 退出登录，并且将当前的 url 保存
  */
 const loginOut = async () => {
-  await outLogin();
+  setToken('')
   const {search, pathname} = history.location;
   const urlParams = new URL(window.location.href).searchParams;
   /** 此方法会跳转到 redirect 参数所在的位置 */
@@ -103,8 +103,12 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({menu}) => {
   return (
     <HeaderDropdown overlay={menuHeaderDropdown}>
       <span className={`${styles.action} ${styles.account}`}>
-        <Avatar size="small" className={styles.avatar}
-                src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png" alt="avatar"/>
+        <Avatar
+          size="small"
+          className={styles.avatar}
+          src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
+          alt="avatar"
+        />
         <span className={`${styles.name} anticon`}>{currentUser.username}</span>
       </span>
     </HeaderDropdown>
